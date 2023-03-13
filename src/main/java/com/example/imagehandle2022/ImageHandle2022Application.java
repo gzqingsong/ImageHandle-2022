e0a8233ceef3b3027ec38595b8ab44db1d090f06
gzqingsong <hbjszt07@163.com>
2023-03-13 14:57:06 +0800

    Adding code for image

---
+++ src/main/java/com/example/imagehandle2022/ImageHandle2022Application.java
@@ -0,0 +1,13 @@
+package com.example.imagehandle2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHandle2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageHandle2022Application.class, args);
+	}
+
+}
