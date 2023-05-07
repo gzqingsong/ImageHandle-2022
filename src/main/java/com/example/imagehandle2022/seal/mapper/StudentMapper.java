4cc21334f9d14204874285f26b063446988951e8
gzqingsong <gzjszt07@163.com>
2023-05-07 15:51:55 +0800

    Adding query student info code

--- src/main/java/com/example/imagehandle2022/seal/mapper/StudentMapper.java
+++ src/main/java/com/example/imagehandle2022/seal/mapper/StudentMapper.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface StudentMapper {
     int insertStudentList(List<StudentDO> studentDOList);
+    StudentDO queryStudent(String studentId);
 }
--- src/main/java/com/example/imagehandle2022/seal/service/IExcelDBService.java
+++ src/main/java/com/example/imagehandle2022/seal/service/IExcelDBService.java
@@ -3,6 +3,6 @@ package com.example.imagehandle2022.seal.service;
 import com.example.imagehandle2022.entity.StudentDO;
 import java.util.List;
 
-public interface ExcelDBService {
+public interface IExcelDBService {
     int insertDB(List<StudentDO> studentDOList);
 }
