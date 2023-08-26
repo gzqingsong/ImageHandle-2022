77d2e08ba2bc85f7942500ffbdbd69bf0c07d9d5
gzqingsong <gzjszt07@163.com>
2023-08-26 11:53:34 +0800

    Adding code for login

--- src/main/java/com/example/imagehandle2022/seal/controller/LoginController.java
+++ src/main/java/com/example/imagehandle2022/seal/controller/LoginController.java
@@ -3,10 +3,14 @@ package com.example.imagehandle2022.seal.controller;
 import com.example.imagehandle2022.seal.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.imagehandle2022.entity.UserDTO;
 import com.example.imagehandle2022.entity.ResultEntity;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -15,7 +19,7 @@ public class LoginController {
     private IUserService userService;
 
     @PostMapping("/login")
-    public ResultEntity login(UserDTO userDTO){
+    public ResultEntity login(@RequestBody UserDTO userDTO){
         ResultEntity result = userService.login(userDTO);
         return result;
     }
